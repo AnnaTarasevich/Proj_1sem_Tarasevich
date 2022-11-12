10a71c583eface9ed481df8b1c0ccac32986a326
AnnaTarasevich <114433495+AnnaTarasevich@users.noreply.github.com>
2022-11-12 17:33:31 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -1,15 +1,16 @@
-# Составить функцию, которая выполнит суммирования числового ряда
+# Составить функцию, которая выполнит суммирования числового ряда.
 try:
-  # y = int(input("введите первое чсло ряда: "))
   x = int(input("Введите последнее число: "))
   x = int(x)
 except ValueError:
   print("Вы ввели не тот тип данных, введите заново: ")
-  # y = int(input("введите первое чсло ряда: "))
   x = int(input("Введите последнее число: "))
-s=0
-def sum_numbers(s):
- for i in range(1,x+1):
-    s=i+s
- return s
-print("Сумма ряда: ", sum_numbers(s))
+def sum_numbers(x):
+    z = 1
+    s = 0
+    while z <= x:
+        s = z + s
+        z += 1
+ # for i in range(1,x+1):
+    return s
+print("Сумма ряда: ", sum_numbers(x))
